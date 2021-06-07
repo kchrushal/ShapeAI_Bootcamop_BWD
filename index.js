7f4631860327834becf5087709162341feeb9b9c
Hrushal K C <72307729+kchrushal@users.noreply.github.com>
2021-06-08 01:59:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Shape AI</h1>
+  </div>,
+  document.getElementById("app")
+);
+
+ReactDOM.render(
+  <div>
+    <h2>Javascript and React.js</h2>
+    <h4>
+      This was an amazing bootamp taken up by Shaurya Sinha we covered
+      everything from Scratch including Javascript,React.js,HTML
+    </h4>
+  </div>,
+  document.getElementById("main")
+);
+
+ReactDOM.render(
+  <div>
+    <h2>Javascript and React.js</h2>
+    <h4>
+      This was an amazing bootamp taken up by Shaurya Sinha we covered
+      everything from Scratch including Javascript,React.js,HTML
+    </h4>
+  </div>,
+  document.getElementById("root")
+);
+
+ReactDOM.render(
+  <div>
+    <h2>Javascript and React.js</h2>
+    <h4>
+      This was an amazing bootamp taken up by Shaurya Sinha we covered
+      everything from Scratch including Javascript,React.js,HTML
+    </h4>
+  </div>,
+  document.getElementById("sub")
+);
